9a62c3967d11c0c1f5369089af3f580774edcb7c
Game <graziellagame@hotmail.com>
2022-01-26 15:09:33 +0700

    .

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React , { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
@@ -6,38 +6,73 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-
+import {db} from "../firebase-config"
+import {
+  collection,
+  addDoc,
+  doc,
+} from "firebase/firestore";
 
 
 
 function AddUser() {
+    const [userx, setUserx] = useState()
+    const [passx, setPassx] = useState()
+    const [rolex, setRolex] = useState()
+
+
+    const getUserx = (e) => {
+        setUserx(e.target.value) 
+      }
+      const getPassx = (e) => {
+        setPassx(e.target.value) 
+      }
+      const getRolex = (e) => {
+        setRolex(e.target.value) 
+      }
+    
+      const handleSubmit = async (e) =>  {
+        e.preventDefault();
+
+        await addDoc(collection(db, "user"), {
+          user:userx,
+          password:passx,
+          role:rolex,
+        }).then((res) => {console.log('User ed') 
+        setUserx("")
+        setPassx("")
+              
+        })
+        .catch((err) => {console.log(err)})
+      };
+
     return (
         <div >
             <div >
                 <div className='setMid'><br />
                     <FormControl sx={{ m: 1, width: '50ch' }} variant="outlined">
-                        <TextField label="User" id="" defaultValue="" /> </FormControl><br />
+                        <TextField label="User" id="userx"  value={userx} onChange={getUserx} /> </FormControl><br />
 
 
                     <FormControl sx={{ m: 1, width: '50ch' }} variant="outlined">
-                        <TextField label="Password" id="" defaultValue="" /> </FormControl><br />
+                        <TextField label="Password" id="passx" value={passx} onChange={getPassx} /> </FormControl><br />
 
                     <FormControl sx={{ m: 1, width: '50ch' }} variant="outlined">
                         <InputLabel id="demo-simple-select-label">Role</InputLabel>
                         <Select
                             labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            //value={age}
-                            //label="Age"
-                            //onChange={handleChange}
+                            id="rolex"
+                            value={rolex}
+                            label="Role"
+                            onChange={getRolex}
                         >
-                            <MenuItem value={10}>Admin</MenuItem>
-                            <MenuItem value={20}>User</MenuItem>
-                            
+                            <MenuItem value="admin">admin</MenuItem>
+                            <MenuItem value="user">user</MenuItem>
+
                         </Select>
                     </FormControl><br />
 
-                    <Button  variant="contained" sx={{ m: 1, width: '20ch' }}>Save</Button>
+                    <Button variant="contained" sx={{ m: 1, width: '20ch' }} onClick={handleSubmit}>Save</Button>
 
                 </div>
             </div>
